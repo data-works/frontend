a9ac140257f8dbd28e9fec3956802f137874b701
Matthew Sinex <mtsinex2695@eagle.fgcu.edu>
2016-03-29 15:56:04 -0400

    searching now allows a space between words for more specific queries
    added ability to use a space in the search bar.
    previously, if one was to type, "Edan" in the search bar, it would
    produce the result. "Edan Vargas" would not produce any results. Now,
    "Edan Vargas" will work as a search. so will "Vargas Edan"

--- app/controllers/employee/EmployeesCtrl.js
+++ app/controllers/employee/EmployeesCtrl.js
@@ -94,6 +94,29 @@ angular.module('Dataworks.controllers', ['ngRoute', 'ngMaterial', 'ng-mfb'])
             templateUrl: './views/addEmployee.html'
         });
     };
+})
+
+/**
+ * Search multiple fields separated by a space
+ */
+    .filter('filterBy', function() {
+    return function(array, query) {
+
+        var parts = query && query.trim().split(/\s+/),
+            keys = Object.keys(array[0]);
+
+        if (!parts || !parts.length) {
+            return array;
+        }
+
+        return array.filter(function(obj) {
+            return parts.every(function(part) {
+                return keys.some(function(key) {
+                    return String(obj[key]).toLowerCase().indexOf(part.toLowerCase()) > -1;
+                });
+            });
+        });
+    };
 });
 
 /**
